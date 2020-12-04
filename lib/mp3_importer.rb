6284c01678b4a6b5c34e4de5b214e9e9014159d0
Michael Fuget <michaelfuget@gmail.com>
2020-12-04 04:39:42 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -1,7 +1,8 @@
 class MP3Importer
   def initialize(folder)
     names = Dir.entries(folder)
-    names = names.select{|x| x.last == "3"} 
+    names = names.select{|x| x ~= /.*3/}
+    puts names
     
 
   end 
