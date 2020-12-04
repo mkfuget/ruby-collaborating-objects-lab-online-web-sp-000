eddeac211ea1aa9eb48f4feb64639a2fc1e585d3
Michael Fuget <michaelfuget@gmail.com>
2020-12-04 04:42:42 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -2,7 +2,10 @@ class MP3Importer
   def initialize(folder)
     names = Dir.entries(folder)
     names = names.select{|x| x.match(/.*3/) != nil}
-    puts names
+    names.each do |file_data|
+      data = file_data.split(" - ")
+      song_name = data[1]
+      artist_name = data[0]
     
 
   end 
